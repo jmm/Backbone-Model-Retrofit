d034b70501793a39dfce485da33e36f750a52504
Jesse McCarthy <git_commits@jessemccarthy.net>
2012-07-21 18:21:14 -0400

    Cleaning up and commenting.

--- rsrc/backbone-retrofit.js
+++ rsrc/backbone-retrofit.js
@@ -1,36 +1,80 @@
-var Backbone_Mod = {};
+/**
+ * Retrofit legacy code to function as Backbone.Model subclasses.
+ */
+
+var Backbone_Retrofit = {};
 
 
-Backbone_Mod.mod_classes = function ( container, klasses, base ) {
+/**
+ * Modify legacy classes to serve as Backbone.Model subclasses.
+ *
+ * @param object container Container for classes being modified.
+ *
+ * @param array klasses Class names within container to modify.
+ *
+ * @param object base Base class to .extend().
+ *
+ * @return void
+ */
+
+Backbone_Retrofit.mod_classes = function ( container, klasses, base ) {
+
+  // Mutation functions to run on legacy classes.
 
   var mutators = [
 
+    /**
+     * Assign return value of mod_class() to the original class's slot in
+     * container.
+     *
+     * @param object container Container for classes being modified.
+     *
+     * @param object klass Name of class within container to modify.
+     *
+     * @return void
+     */
+
     function ( container, klass ) {
 
-      container[ klass ] = Backbone_Mod.mod_class( container[ klass ], base );
+      container[ klass ] = Backbone_Retrofit.mod_class(
+
+        container[ klass ], base
+
+      );
 
     },
 
 
+    /**
+     * JMMDEBUG
+     */
+
     function ( container, klass ) {
 
-      Object.keys( container[ klass ].prototype.defaults ).forEach( function( key, prop ) {
+      Object.keys( container[ klass ].prototype.defaults ).forEach(
 
-        prop = container[ klass ].prototype.defaults[ key ];
+        function( key, prop ) {
 
-        if ( prop && prop.backbone ) {
+          prop = container[ klass ].prototype.defaults[ key ];
 
-          container[ klass ].prototype.defaults[ key ] = prop.backbone.prototype;
+          if ( prop && prop.backbone ) {
+
+            container[ klass ].prototype.defaults[ key ] = prop.backbone.prototype;
+
+          }
+          // if
 
         }
-        // if
 
-      } );
+      );
+      // forEach
 
     }
 
   ];
 
+
+  // Run each mutator fucntion for each input class.
 
   mutators.forEach( function ( mutator ) {
 
@@ -45,68 +89,44 @@ Backbone_Mod.mod_classes = function ( container, klasses, base ) {
 };
 
 
-Backbone_Mod.mod_class = function ( klass, base ) {
+/**
+ * Modify a legacy class to serve as a Backbone.Model subclass.
+ *
+ * @param object klass Legacy class.
+ *
+ * @param object base Base class to call .extend() on.
+ *
+ * @return object Modified klass.
+ */
 
-  var backbone_proto = { 'attrs' : {}, 'methods' : {} };
+Backbone_Retrofit.mod_class = function ( klass, base ) {
 
-  var stubs = {};
+  // Separate attrs and methods.
 
-
-  stubs.get_set = function ( meth, key ) {
-
-    var args = Array.prototype.slice.call( arguments, 1 );
-
-    return function () {
-
-      return base.prototype[ meth ].apply(
-
-        this,
-
-        args.concat( Array.prototype.slice.call( arguments ) )
-
-      );
-
-    };
-
-  };
+  var backbone_proto = { defaults : {} };
 
 
-  stubs.ctor = function ( klass ) {
-
-    return function () {
-
-      if ( ! this.cid ) {
-
-        base.apply( this, arguments );
-
-      }
-      // if
-
-
-      klass.apply( this, arguments );
-
-    };
-
-  };
-
+  // backbone_proto | backbone_proto.defaults
 
   var collection;
 
-  Object.keys( klass.prototype ).forEach( function ( value, key, array ) {
 
-    key = value;
+  // Assign each klass.prototype member to the appropriate collection
 
-    value = klass.prototype[ value ];
+  Object.keys( klass.prototype ).forEach( function ( prop ) {
 
-    collection = value instanceof Function ? 'methods' : 'attrs';
+    var value = klass.prototype[ prop ];
+
+    collection = value instanceof Function ?
+
+      backbone_proto :
+
+      backbone_proto.defaults;
 
 
-    backbone_proto[ collection ][ key ] = value;
+    collection[ prop ] = value;
 
   } );
-
-
-  backbone_proto.methods.defaults = backbone_proto.attrs;
 
 
   if ( klass.prototype.parent && klass.prototype.parent.backbone ) {
@@ -120,17 +140,20 @@ Backbone_Mod.mod_class = function ( klass, base ) {
 
   if ( klass.prototype.hasOwnProperty( 'constructor' ) ) {
 
-    backbone_proto.methods.initialize = klass.prototype.constructor;
+    // Copy legacy class constructor to Backbone.Model.initialize.
+
+    backbone_proto.initialize = klass.prototype.constructor;
+
+
+    // Copy parent class defaults to current class.
 
     if ( klass.prototype.parent && klass.prototype.parent.defaults ) {
 
-      backbone_proto.methods.defaults = _.extend(
+      backbone_proto.defaults = _.defaults(
 
-        {},
+        backbone_proto.defaults,
 
-        klass.prototype.parent.defaults,
-
-        backbone_proto.methods.defaults
+        klass.prototype.parent.defaults
 
       );
 
@@ -143,26 +166,35 @@ Backbone_Mod.mod_class = function ( klass, base ) {
 
   var old_klass = klass;
 
-  klass = base.extend( backbone_proto.methods );
+  klass = base.extend( backbone_proto );
 
   old_klass.prototype.backbone = klass;
 
 
-  if ( backbone_proto.attrs.hasOwnProperty( 'id' ) ) {
+  // Properties that may be used in legacy classes and are special-cased by
+  // Backbone (treated as regular props not attrs)
 
-    delete backbone_proto.attrs.id;
-
-  }
-  // if
+  var special_props = [ 'id' ];
 
 
-  Object.keys( backbone_proto.attrs ).forEach( function ( key, index, attrs ) {
+  // Prevent special props from being treated as attrs with getters / setters.
+
+  special_props.forEach( function ( prop ) {
+
+    delete klass.prototype[ prop ];
+
+  } );
+
+
+  // Create getters and setters for legacy props that will be treated as attrs.
+
+  Object.keys( backbone_proto.defaults ).forEach( function ( key ) {
 
     Object.defineProperty( klass.prototype, key, {
 
-      get : stubs.get_set( 'get', key ),
+      get : Backbone_Retrofit.get_set( 'get', key ),
 
-      set : stubs.get_set( 'set', key )
+      set : Backbone_Retrofit.get_set( 'set', key )
 
     } );
 
@@ -173,3 +205,33 @@ Backbone_Mod.mod_class = function ( klass, base ) {
 
 };
 // mod_class
+
+
+/**
+ * Manufacture a getter|setter function for an attr.
+ *
+ * @param string meth get|set
+ *
+ * @param string key Attr name
+ *
+ * @return function getter|setter
+ */
+
+Backbone_Retrofit.get_set = function ( meth, key ) {
+
+  var args = Array.prototype.slice.call( arguments, 1 );
+
+  return function () {
+
+    return base.prototype[ meth ].apply(
+
+      this,
+
+      args.concat( Array.prototype.slice.call( arguments ) )
+
+    );
+
+  };
+
+};
+// get_set
